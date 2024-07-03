144cd94b60a14f19dce6fd3e2ca698e26863416c
Vasile Marin <vmarin@c2r4s7.42london.com>
2024-07-03 19:07:55 +0100

    start on input sanitization

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:09:19 by vmarin            #+#    #+#             */
-/*   Updated: 2024/07/02 19:04:56 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/07/03 19:07:06 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,17 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -47,5 +58,50 @@ int	main(int argc, char *argv[])
 	{
 		exit(1);
 	}
-	printf("%s", argv[1]);
+	int	i = 1;
+	while (i < argc)
+	{
+		int	arg_len = ft_strlen(argv[i]);
+		if (arg_len > 10)
+		{
+			write(2, "Error\n", 6);
+			exit(1);
+		}
+		int	j = 0;
+		while (j < arg_len)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				write(2, "Error\n", 6);
+				exit(1);
+			}
+			j++;
+		}
+		j = i + 1;
+		while (j < argc)
+		{
+			int	len_i = ft_strlen(argv[i]);
+			int	len_j = ft_strlen(argv[j]);
+			if (len_i < len_j || len_i > len_j)
+				continue;
+			else 
+			{
+				int	k = 0;
+				while (k < len_i)
+				{
+					if (argv[i][k] != argv[j][k])
+						break;
+					k++;
+				}
+				if (k == len_i)
+				{
+					write(2, "Error\n", 6);
+					exit(1);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("groovy");
 }
